b38c93a2e95230827e6b767d704e197be123fd8b
davidlei <davidleitw@gmail.com>
2021-09-23 13:38:23 +0800

    update method name

--- thoth/mainWindow.py
+++ thoth/mainWindow.py
@@ -39,11 +39,11 @@ class MainWindow(QMainWindow):
 
     def treeContextMenu(self, pos):
         self.contextMenu = QMenu()
-        self.actionA = self.contextMenu.addAction(u'动作')  # 添加动作
-        self.actionB = self.contextMenu.addAction(u'动作b')
+        self.actionA = self.contextMenu.addAction(u'Method A')
+        self.actionB = self.contextMenu.addAction(u'Method B')
         self.actionA.triggered.connect(self.actionHandler)
-        self.contextMenu.exec_(self.mapToGlobal(pos))  # 随指针的位置显示菜单
-        self.contextMenu.show()  # 显示
+        self.contextMenu.exec_(self.mapToGlobal(pos))
+        self.contextMenu.show()
 
     def actionHandler(self):
         for idx in self.tree.selectedIndexes():
@@ -54,14 +54,14 @@ class MainWindow(QMainWindow):
 
         OpenFileButton = QPushButton("Open File")
         OpenFolderButton = QPushButton("Open Folder")
+        OpenFileButton.clicked.connect(self.btn_chooseFile)
+        OpenFolderButton.clicked.connect(self.btn_chooseFolder)
 
         layout = QVBoxLayout()
         layout.addWidget(OpenFileButton)
         layout.addWidget(OpenFolderButton)
         layout.addStretch(1)
 
-        OpenFileButton.clicked.connect(self.btn_chooseFile)
-        OpenFolderButton.clicked.connect(self.btn_chooseFolder)
         self.leftFunctionField.setLayout(layout)
 
     def btn_chooseFile(self):
