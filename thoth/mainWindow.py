2b282759fd9dcc5f3d0302f25fcbbc5331e2cac6
davidlei <davidleitw@gmail.com>
2021-10-07 18:56:01 +0800

    rename: createTreeContextMenu

--- thoth/mainWindow.py
+++ thoth/mainWindow.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
     QVBoxLayout, QStyleFactory, QHBoxLayout, QTreeView, QFileSystemModel, \
     QMenu
 
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -20,53 +21,9 @@ class MainWindow(QMainWindow):
         self.createRightFileTree()
         self.setMainLayout()
 
-    def setMainLayout(self) -> None:
-        mainLayout = QHBoxLayout()
-        mainLayout.addLayout(self.leftLayout)
-        mainLayout.addWidget(self.rightTree)
-
-        w = QWidget()
-        w.setLayout(mainLayout)
-        self.setCentralWidget(w)
-
-    def createRightFileTree(self) -> None:
-        self.model = QFileSystemModel()
-        self.model.setRootPath("")
-        self.rightTree = QTreeView()
-        self.rightTree.setModel(self.model)
-        self.rightTree.setRootIndex(self.model.index(""))
-        self.rightTree.setColumnWidth(0, 250)
-        self.rightTree.setAlternatingRowColors(True)
-        self.rightTree.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.rightTree.customContextMenuRequested.connect(self.treeContextMenu)
-
     def loadInitialSetting(self) -> None:
         QApplication.setStyle(QStyleFactory.create("Fusion"))
         self.setWindowTitle("file manager")
-
-    def treeContextMenu(self, pos) -> None:
-        self.contextMenu = QMenu()
-        self.contextMenu.addAction(u'add').triggered.connect(self.addHandler)
-        self.contextMenu.addAction(u'rename').triggered.connect(self.renameHandler)
-        self.contextMenu.addAction(u'backup').triggered.connect(self.backupHandler)
-        self.contextMenu.exec_(self.mapToGlobal(pos))
-        self.contextMenu.show()
-
-    def getFilePath(self) -> str:
-        return self.model.filePath(self.rightTree.currentIndex())
-
-    def addHandler(self) -> None:
-        path = self.getFilePath()
-        self.rightTree.setRootIndex(self.model.index(path))
-        print(path)
-
-    def renameHandler(self) -> None:
-        path = self.getFilePath()
-        print(path)
-
-    def backupHandler(self) -> None:
-        path = self.getFilePath()
-        print(path)
 
     def createLeftField(self) -> None:
         leftField = QGroupBox("toolbar")
@@ -90,6 +47,54 @@ class MainWindow(QMainWindow):
 
         self.leftLayout = QVBoxLayout()
         self.leftLayout.addWidget(leftField)
+
+    def createRightFileTree(self) -> None:
+        self.model = QFileSystemModel()
+        self.model.setRootPath("")
+        self.rightTree = QTreeView()
+        self.rightTree.setModel(self.model)
+        self.rightTree.setRootIndex(self.model.index(""))
+        self.rightTree.setColumnWidth(0, 250)
+        self.rightTree.setAlternatingRowColors(True)
+        self.rightTree.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.rightTree.customContextMenuRequested.connect(self.createTreeContextMenu)
+
+    def setMainLayout(self) -> None:
+        mainLayout = QHBoxLayout()
+        mainLayout.addLayout(self.leftLayout)
+        mainLayout.addWidget(self.rightTree)
+
+        w = QWidget()
+        w.setLayout(mainLayout)
+        self.setCentralWidget(w)
+
+    def createTreeContextMenu(self, pos) -> None:
+        self.contextMenu = QMenu()
+        self.contextMenu.addAction(u'add').triggered.connect(self.addHandler)
+        self.contextMenu.addAction(u'rename').triggered.connect(self.renameHandler)
+        self.contextMenu.addAction(u'delete').triggered.connect(self.deleteHandler)
+        self.contextMenu.addAction(u'backup').triggered.connect(self.backupHandler)
+        self.contextMenu.exec_(self.mapToGlobal(pos))
+        self.contextMenu.show()
+
+    def getFilePath(self) -> str:
+        return self.model.filePath(self.rightTree.currentIndex())
+
+    def addHandler(self) -> None:
+        path = self.getFilePath()
+        self.rightTree.setRootIndex(self.model.index(path))
+        print(path)
+
+    def renameHandler(self) -> None:
+        path = self.getFilePath()
+        print(path)
+
+    def deleteHandler(self) -> None:
+        pass
+
+    def backupHandler(self) -> None:
+        path = self.getFilePath()
+        print(path)
 
     def btn_chooseFile(self):
         (filePath, fileType) = QFileDialog.getOpenFileName(self, "file", "/")
